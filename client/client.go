e4b29794798d750d211caa01d07445caa3dca49b
shenchangqing <shenqinpeng@126.com>
2022-10-12 17:48:58 +0800

    Add client with kubeconfig context

--- client/client.go
+++ client/client.go
@@ -10,6 +10,7 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
+	"k8s.io/klog/v2"
 	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -49,6 +50,48 @@ func GetRuntimeClient(r *runtime.Scheme) runtimeclient.Client {
 	return client
 }
 
+func GetDynamicClientWithContext(contextName string) dynamic.Interface {
+	var kubeconfig string
+	var config *rest.Config
+
+	var err error
+	if os.Getenv("KUBECONFIG") != "" {
+		kubeconfig = filepath.Join(os.Getenv("KUBECONFIG"))
+	} else if home := homedir.HomeDir(); home != "" {
+		kubeconfig = filepath.Join(home, ".kube", "config")
+	} else {
+		kubeconfig = ""
+	}
+
+	if fileExist(kubeconfig) {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if contextName != "" {
+			configLoadingRules := &clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfig}
+			configOverrides := &clientcmd.ConfigOverrides{CurrentContext: contextName}
+			config, err = clientcmd.NewNonInteractiveDeferredLoadingClientConfig(configLoadingRules, configOverrides).ClientConfig()
+
+			if err != nil {
+				klog.Errorf("Switch kubeconfig context err: ", err)
+			}
+		}
+		if err != nil {
+			panic(err.Error())
+		}
+	} else {
+		// creates the in-cluster config
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	// creates the clientset, default behavor
+
+	dynaClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	return dynaClient
+}
 func GetDynamicClient() dynamic.Interface {
 	var kubeconfig string
 	var config *rest.Config
@@ -83,6 +126,48 @@ func GetDynamicClient() dynamic.Interface {
 	return dynaClient
 }
 
+func GetClientWithContext(contextName string) *kubernetes.Clientset {
+	var kubeconfig string
+	var config *rest.Config
+
+	var err error
+	if os.Getenv("KUBECONFIG") != "" {
+		kubeconfig = filepath.Join(os.Getenv("KUBECONFIG"))
+	} else if home := homedir.HomeDir(); home != "" {
+		kubeconfig = filepath.Join(home, ".kube", "config")
+	} else {
+		kubeconfig = ""
+	}
+
+	if fileExist(kubeconfig) {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if contextName != "" {
+			configLoadingRules := &clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfig}
+			configOverrides := &clientcmd.ConfigOverrides{CurrentContext: contextName}
+			config, err = clientcmd.NewNonInteractiveDeferredLoadingClientConfig(configLoadingRules, configOverrides).ClientConfig()
+
+			if err != nil {
+				klog.Errorf("Switch kubeconfig context err: ", err)
+			}
+		}
+		if err != nil {
+			panic(err.Error())
+		}
+	} else {
+		// creates the in-cluster config
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	// creates the clientset, default behavor
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	return clientset
+}
 func GetClient() *kubernetes.Clientset {
 	var kubeconfig string
 	var config *rest.Config
--- go.mod
+++ go.mod
@@ -46,7 +46,7 @@ require (
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	k8s.io/api v0.24.2 // indirect
-	k8s.io/klog/v2 v2.60.1 // indirect
+	k8s.io/klog/v2 v2.60.1
 	k8s.io/kube-openapi v0.0.0-20220328201542-3ee0da9b0b42 // indirect
 	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
 	sigs.k8s.io/controller-runtime v0.12.2
